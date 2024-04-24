12cb1c3bf91bb791fe51e25e3e3cd0ce49b240fe
Winnery05 <elmedhassan2002@gmail.com>
2024-04-24 23:57:48 +0100

    fix routes bug

--- routes/api.php
+++ routes/api.php
@@ -9,20 +9,22 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\ProjectController;
 
-
-//get all data => projects , admins , tasks ,supervisors , users ( NB data must be pluriel)
-Route::get('/data/{data}', [Controller::class, 'index']);
-Route::get('/data/{data}/{id}', [Controller::class, 'show']);
-
-//CRUD all profiles Routes
-Route::apiResource('profiles', ProfileController::class);
-Route::post('/register', [ProfileController::class,'register']);
-Route::POST('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::POST('/logout', [AuthController::class, 'logout']);
     Route::GET('/user', [AuthController::class, 'user']);
 });
+
+Route::post('/register', [ProfileController::class,'register']);
+Route::POST('/login', [AuthController::class, 'login']);
+
+
+//get all data => projects , admins , tasks ,supervisors , users ( NB data must be pluriel)
+Route::get('/{data}', [Controller::class, 'index']);
+Route::get('/{data}/{id}', [Controller::class, 'show']);
+
+//CRUD all profiles Routes
+Route::apiResource('profiles', ProfileController::class);
+
 //Route::apiResource('offers', OfferController::class);
 
 //Offers
